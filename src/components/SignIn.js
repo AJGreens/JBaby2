63598f18981080d0634de6b0febc8c1ce1d2e54e
Jordan Altomare <jaltomar@u.rochester.edu>
2021-08-23 13:35:36 -0400

    yeehaw

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,5 +1,5 @@
 import React,{useState,useContext,useRef} from 'react'
-import {Card,Form,Alert,Button} from 'react-bootstrap'
+import {Card,Form,Alert,Button, Row, Col} from 'react-bootstrap'
 import { AuthContext } from '../contexts/AuthContext'
 import {Link,useHistory} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
@@ -40,43 +40,68 @@ function SignIn(){
 
 
     return(
-        <div className = 'everythingDiv'>
-            <div className = 'titleDiv'>
-                <div className = 'imgDiv' style = {{display: 'inline-block'}} >
-                    <img className = 'logoCirc' src = {logocirc} alt = 'logocirc'/>
-                </div>
-                    <h1 style = {{fontSize: '40pt', display: 'inline-block'}}>HealthFull</h1>
-                <br/>
-                <br/>
-                <h4 className = 'text-center'>Eat Healthy.  Feel Full.  Start tracking your healthy food choices today.</h4>
-            </div>
-            <Container style = {{maxWidth: '400px', marginRight: '100px'}} >
-                <div style = {{width: '100%'}}> 
-                    <Card style = {{background: 'linear-gradient( rgba(0, 0, 0, .4),  rgba(0, 0, 0, .4))', minWidth: '400px', border: 'white solid 0px'}}>
-                        <Card.Body className = 'cardBody'>
-                            <Form onSubmit={handleSubmit}>
-                                <h3 className="text-center mb-3">Sign In</h3>
-                                {error&& <Alert variant="danger">{error}</Alert>}
-                                <Form.Group className="mb-3">
-                                    <Form.Label>Email</Form.Label>
-                                    <Form.Control type="email" ref={emailRef}/>
-                                </Form.Group>
-                                <Form.Group className="mb-3">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" ref={passwordRef}/>
-                                </Form.Group>
-                                <div className = 'text-center'>
-                                    <Button className="mb-3" style={{width:"50%"}} type="submit" variant="primary">Sign In</Button>
-                                    <br/>
-                                    <Button style={{width:"50%"}} onClick={handleGoogle} variant="success">Google</Button>
-                                    <p className = 'text-center mt-2'>Need an account? <Link to = '/signup' style = {{color: 'orange'}}>Sign Up</Link> </p>
-                                </div>
-                            </Form>
-                        </Card.Body>
-                    </Card>
-                </div>
+
+        <div className = 'd-flex align-items-center everyDiv' style = {{height: '100vh'}}>
+
+
+            <Container fluid>
+                <Row>
+                    <Col className = 'my-5' style = {{ textAlign: 'center'}}>
+                        <h1>
+                            Healthfull
+                        </h1>
+                    </Col>
+                    <Col>
+                        <Container id='myContainer'>
+                            <Card id = 'myCard'>
+                                <Card.Body className = 'cardBody'>
+                                    <Form onSubmit={handleSubmit}>
+                                        <h3 className="text-center mb-3">Sign In</h3>
+                                        {error&& <Alert variant="danger">{error}</Alert>}
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Email</Form.Label>
+                                            <Form.Control type="email" ref={emailRef}/>
+                                        </Form.Group>
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Password</Form.Label>
+                                            <Form.Control type="password" ref={passwordRef}/>
+                                        </Form.Group>
+                                        <div className = 'text-center'>
+                                            <Button className="mb-3" style={{width:"50%"}} type="submit" variant="primary">Sign In</Button>
+                                            <br/>
+                                            <Button style={{width:"50%"}} onClick={handleGoogle} variant="success">Google</Button>
+                                            <p className = 'text-center mt-2'>Need an account? <Link to = '/signup' style = {{color: 'orange'}}>Sign Up</Link> </p>
+                                        </div>
+                                    </Form>
+                                </Card.Body>
+                            </Card>
+                        </Container>
+                    </Col>
+                </Row>
+
             </Container>
-      </div>
+
+            
+ 
+        </div>
+
+
+    //     <div className = 'everythingDiv'>
+    //         <div className = 'titleDiv'>
+    //             <div className = 'imgDiv' style = {{display: 'inline-block'}} >
+    //                 <img className = 'logoCirc' src = {logocirc} alt = 'logocirc'/>
+    //             </div>
+    //                 <h1 style = {{fontSize: '40pt', display: 'inline-block'}}>HealthFull</h1>
+    //             <br/>
+    //             <br/>
+    //             <h4 className = 'text-center'>Eat Healthy.  Feel Full.  Start tracking your healthy food choices today.</h4>
+    //         </div>
+    //         <Container style = {{maxWidth: '400px', marginRight: '100px'}} >
+    //             <div style = {{width: '100%'}}> 
+
+    //             </div>
+    //         </Container>
+    //   </div>
 
     )
 }
