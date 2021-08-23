fc9afe14eca44e85289a31c55088b91e2d349d78
Abdelmonem Khedr <bebo@dhcp-10-5-40-246.wireless.rochester.edu>
2021-08-22 20:17:37 -0400

    phone styling done

--- src/components/MyNav.js
+++ src/components/MyNav.js
@@ -12,21 +12,27 @@ function MyNav(props){
     return(
         // style={{height:"7vh",background:"linear-gradient(120deg, rgba(72,151,216,1) 65%, rgba(255,219,92,1) 65%)"}}
         // background:"#2A7EC3"
-        <Navbar  className = 'customNav' variant="dark" style={{height:"7vh",background:"#2A7EC3"}}>
-            <Container >
-                <Nav >
-                    <Navbar.Brand as={Link} to="/">
-                        <img
-                            alt=""
-                            src={logoPic}
-                            width="32"
-                            height="30"
-                            className="d-inline-block align-top"
-                        />App Name</Navbar.Brand>
-                    <Nav.Link as={Link} active={props.dActive}  to="/">Dashboard</Nav.Link>
-                    <Nav.Link as={Link} active={props.sActive} to="/summary">Summary </Nav.Link>
-                </Nav>
-                <Button variant="outline-light" onClick={signOut}>Sign Out</Button>
+        // style={{height:"7vh",background:"#2A7EC3"}}
+        <Navbar expand="lg" className = 'customNav' style={{background:"#2A7EC3"}} variant="dark" >
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                            <img
+                                alt=""
+                                src={logoPic}
+                                width="32"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />App Name
+                </Navbar.Brand>
+                <Navbar.Toggle/>
+                <Navbar.Collapse>
+                    <Nav>
+                        <Nav.Link eventKey="1" as={Link} active={props.dActive}  to="/">Dashboard</Nav.Link>
+                        <Nav.Link eventKey="2"as={Link} active={props.sActive} to="/summary">Summary </Nav.Link>
+                    </Nav>
+                    <Button  className= "ms-auto" variant="outline-light" onClick={signOut}>Sign Out</Button>
+                </Navbar.Collapse>
+                
             </Container>
         </Navbar>    
     )
