b2254d6a6e32ed3628924704df8fa8ae4b30e9cf
Abdelmonem Khedr <bebo@dhcp-10-5-38-28.wireless.rochester.edu>
2021-09-13 20:05:35 -0400

    test bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'jquery';
+import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
