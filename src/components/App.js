59430ed1cfaeee44cc38a593511d0dc26ea0f39f
Abdelmonem Khedr <bebo@dhcp-10-5-35-161.wireless.rochester.edu>
2021-09-17 16:18:24 -0400

    added meta tag

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from'react';
+import DocumentMeta from 'react-document-meta';
 import SignUp from './SignUp'
 import SignIn from './SignIn';
 import Dashboard from './Dashboard'
@@ -9,21 +10,30 @@ import {HashRouter as Router,Switch} from 'react-router-dom'
 import Summary from './Summary'
 
 function App() {
+  const meta = {
+    title: 'Platesome',
+    description: 'Platesome is a nutrition tracking website designed to keep track of your daily servings of fruits, vegetables, and dairy!  Track your progress and become healthier by eating your ideal daily servings.',
+    canonical: 'https://platesome.com/',
+    meta: {
+      charset: 'utf-8',
+      name: {
+        keywords: 'react,meta,document,html,tags'
+      }
+    }
+  };
   return (
-  <div>
-    
-    <AuthProvider>
-      <Router>
-        <Switch>
-          <PrivateRouteMain path='/' exact component={Dashboard}/>
-          <PrivateRouteAccount path='/signin' exact component={SignIn}/>
-          <PrivateRouteAccount path='/signup' exact component={SignUp}/>
-          <PrivateRouteMain path = '/summary' exact component = {Summary}/>
-        </Switch>
-      </Router>
-    </AuthProvider>
-
-  </div>
+  <DocumentMeta {...meta}>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <PrivateRouteMain path='/' exact component={Dashboard}/>
+            <PrivateRouteAccount path='/signin' exact component={SignIn}/>
+            <PrivateRouteAccount path='/signup' exact component={SignUp}/>
+            <PrivateRouteMain path = '/summary' exact component = {Summary}/>
+          </Switch>
+        </Router>
+      </AuthProvider>
+  </DocumentMeta>
   
   );
 }
