eb391ad4f5b5f97a436ec499d90175e7e0e54d18
Jordan Altomare <jaltomar@u.rochester.edu>
2022-09-25 12:07:38 -0400

    meaningless comment test

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,27 @@
-import React from'react';
-import SignUp from './SignUp'
-import SignIn from './SignIn';
-import Dashboard from './Dashboard'
-import PrivateRouteMain from './PrivateRouteMain';
-import PrivateRouteAccount from './PrivateRouteAccount';
-import {AuthProvider} from '../contexts/AuthContext'
-import {HashRouter as Router,Switch} from 'react-router-dom'
-import Summary from './Summary'
+import React from "react";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import Dashboard from "./Dashboard";
+import PrivateRouteMain from "./PrivateRouteMain";
+import PrivateRouteAccount from "./PrivateRouteAccount";
+import { AuthProvider } from "../contexts/AuthContext";
+import { HashRouter as Router, Switch } from "react-router-dom";
+import Summary from "./Summary";
 
 function App() {
+  //meaningless comment test
 
   return (
-  
-      <AuthProvider>
-        
-        <Router>
-          <Switch>
-            <PrivateRouteMain path='/' exact component={Dashboard}/>
-            <PrivateRouteAccount path='/signin' exact component={SignIn}/>
-            <PrivateRouteAccount path='/signup' exact component={SignUp}/>
-            <PrivateRouteMain path = '/summary' exact component = {Summary}/>
-          </Switch>
-        </Router>
-      </AuthProvider>
-  
-  
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <PrivateRouteMain path="/" exact component={Dashboard} />
+          <PrivateRouteAccount path="/signin" exact component={SignIn} />
+          <PrivateRouteAccount path="/signup" exact component={SignUp} />
+          <PrivateRouteMain path="/summary" exact component={Summary} />
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 }
 
